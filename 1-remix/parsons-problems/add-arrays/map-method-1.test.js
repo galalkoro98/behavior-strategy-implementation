ff46a1e1076bc3b3d2c7934eeb88453dfa73ed59
Galalkoro98 <galal.koro@gmail.com>
2022-11-03 23:43:09 +0100

    exercise doen

--- 1-remix/parsons-problems/add-arrays/map-method-1.test.js
+++ 1-remix/parsons-problems/add-arrays/map-method-1.test.js
@@ -3,8 +3,7 @@ const addArraysTest = require('./map-method-1');
 describe('sum of two arrays with the same length', () => {
   it('sum of add arrays', () => {
     // eslint-disable-next-line no-sequences
-    let a1 = [1, 2];
-    let a2 = [4, 5];
-    expect(addArraysTest.addArrays(a1, a2)).toEqual(5, 7);
+
+    expect(addArraysTest.addArrays([1, 2], [4, 5])).toEqual([5, 7]);
   });
 });
