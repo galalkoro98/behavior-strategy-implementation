6d0ee5881a8d5dbcf8b57948b0fa5d540d5653ad
Galalkoro98 <galal.koro@gmail.com>
2022-11-06 23:36:00 +0100

    reverse string exercise done

--- 2-write/1-function-design/exercises/easy/reverse-a-string.test.js
+++ 2-write/1-function-design/exercises/easy/reverse-a-string.test.js
@@ -1,31 +1,77 @@
 // #todo
 
-'use strict';
-
-/**
- * reverses a string
- * @param {string} [toReverse=''] - the string to reverse
+/** ....................
+ * Reverses a string
+ *
+ * @param {string} [_toReverse=''] - the string to reverse
  * @returns {string} the reversed argument
  */
 
 // -------- your solutions --------
+// eslint-disable-next-line no-unused-vars
+const reverseString = (toReverse = "") => {
+  if (typeof toReverse !== "string") {
+    throw TypeError();
+  }
 
-for (const solution of [secretSolution]) {
+  if (toReverse.length > 10) {
+    throw new Error();
+  }
+  let newString = "";
+
+  for (let i = toReverse.length - 1; i >= 0; i--) {
+    // add each character to newString
+    newString += toReverse.charAt(i);
+  }
+  // eslint-disable-next-line max-len
+  if (
+    newString.charAt(newString.length - 1) ===
+    newString.charAt(newString.length - 1).toUpperCase()
+  ) {
+    return newString;
+  }
+
+  return newString;
+};
+
+// eslint-disable-next-line no-restricted-syntax, no-use-before-define
+for (const solution of [secretSolution, reverseString]) {
   // the main test suite for the function
-  describe(solution.name + ': reverses a string', () => {
+  describe(`${solution.name}: reverses a string`, () => {
     it('default parameter is an empty string -> ""', () => {
-      expect(solution()).toEqual('');
+      expect(solution()).toEqual("");
     });
     it('an empty string -> ""', () => {
-      expect(solution('')).toEqual('');
+      expect(solution("")).toEqual("");
     });
-    it('a string with all capital letters', () => {
-      expect(solution('ASDF')).toEqual('FDSA');
+    it("a string with all capital letters", () => {
+      expect(solution("ASDF")).toEqual("FDSA");
     });
     // write at least 5 more tests ...
+    it("a string with all small letters", () => {
+      expect(solution("asdf")).toEqual("fdsa");
+    });
+    it("if to reverse is not an string should throw error", () => {
+      expect(() => solution([])).toThrow(TypeError);
+    });
+    // it("if to reverse string is undefined", () => {
+    //   // eslint-disable-next-line no-undefined
+
+    //   const lengthString = "hello world hello";
+    //   expect(() => solution(lengthString)).toThrow(Error);
+    // });
   });
 }
 
 // minified solution for testing your tests
 // prettier-ignore
-function secretSolution(a = '') { if ("string" != typeof a) throw new TypeError("toReverse is not a string"); return a.split("").reverse().join("") }
+/**
+ * @param a
+ */
+
+function secretSolution(a = '') {
+  if (typeof a !== "string") {
+    throw new TypeError("toReverse is not a string");
+  }
+  return a.split("").reverse().join("");
+}
