8cbbe48dd0495bb34d42a7024749749039334704
Galalkoro98 <galal.koro@gmail.com>
2022-11-06 23:33:52 +0100

    count down exercise solved

--- 2-write/1-function-design/exercises/easy/count-down.test.js
+++ 2-write/1-function-design/exercises/easy/count-down.test.js
@@ -1,9 +1,10 @@
+/* eslint-disable for-direction */
+/* eslint-disable no-restricted-syntax */
 // #todo
 
-'use strict';
-
-/**
- * builds an array counting down from `start` to 0
+/** ....................
+ * Builds an array counting down from `start` to 0
+ *
  * @param {number} [start=0] - the number to count down from
  *  start must be an integer that is greater than 0
  * @returns {number[]} an array of all numbers from `start` to 0
@@ -11,22 +12,61 @@
 
 // -------- your solutions --------
 
-for (const solution of [secretSolution]) {
+const countDown = (start = 0) => {
+  if (typeof start !== "number") {
+    throw Error("is not a number");
+  }
+
+  if (!Number.isInteger(start)) {
+    throw new Error("the number is integer");
+  }
+
+  if (start < 0) {
+    throw new RangeError("the number is less than zero");
+  }
+
+  const startNumber = [];
+
+  for (let i = start; i >= 0; i--) {
+    startNumber.push(i);
+  }
+
+  return startNumber;
+};
+
+// eslint-disable-next-line no-use-before-define
+for (const solution of [secretSolution, countDown]) {
   // the main test suite for the function
-  describe(solution.name + ': counts down to 0', () => {
-    it('default parameter is 0 -> [0]', () => {
+  describe(`${solution.name}: counts down to 0`, () => {
+    it("default parameter is 0 -> [0]", () => {
       expect(solution()).toEqual([0]);
     });
-    it('0 -> [0]', () => {
+    it("0 -> [0]", () => {
       expect(solution(0)).toEqual([0]);
     });
-    it('1 -> [1, 0]', () => {
+    it("1 -> [1, 0]", () => {
       expect(solution(1)).toEqual([1, 0]);
     });
     // write at least 5 more tests ...
+    it("2 -> [2, 1, 0]", () => {
+      expect(solution(2)).toEqual([2, 1, 0]);
+    });
+
+    it("start is not a number should throw Error", () => {
+      expect(() => solution([])).toThrow(Error);
+    });
+    it("start is an integer should throw Error", () => {
+      expect(() => solution(4.33)).toThrow(Error);
+    });
+    it("start is less than 0", () => {
+      expect(() => solution(-2)).toThrow(RangeError);
+    });
   });
 }
 
 // minified solution for testing your tests
 // prettier-ignore
-function secretSolution(a = 0) { if ("number" != typeof a) throw new TypeError("start is not a number"); if (!Number.isInteger(a)) throw new Error("start is not an integer"); if (0 > a) throw new RangeError("start is less than 0"); const b = []; for (let c = a; 0 <= c; c--)b.push(c); return b }
+/**
+ * @param a
+ */
+function secretSolution(a = 0) { if (typeof a !== "number") throw new TypeError("start is not a number"); if (!Number.isInteger(a)) throw new Error("start is not an integer"); if (a < 0) throw new RangeError("start is less than 0"); const b = []; for (let c = a; c >= 0; c--)b.push(c); return b; }
