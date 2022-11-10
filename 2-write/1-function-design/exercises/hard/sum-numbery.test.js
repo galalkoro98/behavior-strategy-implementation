710b1842d047f80352b46d3904c23dc924f74c27
Galalkoro98 <galal.koro@gmail.com>
2022-11-10 22:36:25 +0100

    exercise add

--- 2-write/1-function-design/exercises/hard/sum-numbery.test.js
+++ 2-write/1-function-design/exercises/hard/sum-numbery.test.js
@@ -1,6 +1,7 @@
+/* eslint-disable */
 // #todo
 
-'use strict';
+"use strict";
 
 /**
  * sums all numbery strings in an array of strings
@@ -13,26 +14,39 @@
 // -------- your solutions --------
 
 const mapFilterReduce = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw new TypeError();
+  }
+
+  const noString = arr.some((item) => typeof item !== "string");
+  if (noString) {
+    throw new TypeError();
+  }
   // these work, you need to pass them to the right array methods
-  const isNotNaN = (entry) => !Number.isNaN(entry);
+  /* const isNotNaN = (entry) => !Number.isNaN(entry);
   const sumNumbers = (acc, next) => acc + next;
   const castToNumber = (entry) => Number(entry);
-
+*/
   // fill in the array methods and pass in the correct logic
-  const sumOfNumberies = arr._(_)._(_)._(_, _);
+  const sumOfNumberies = arr
+    .map((element) => element)
+    .filter((item) => !Number.isNaN(item))
+    .reduce((current, previous) => current + previous, 0);
 
   return sumOfNumberies;
 };
 
 // -------- your solutions --------
 
-for (const solution of [
-  secretSolution,
-  // mapFilterReduce,
-]) {
-  describe(solution.name + ': _', () => {
-    describe('_', () => {
-      it('_', () => {});
+for (const solution of [secretSolution, mapFilterReduce]) {
+  describe(solution.name + ": sum all number of strings in array", () => {
+    describe("array of strings return ", () => {
+      it("if array is empty should return 0", () => {
+        expect(solution([])).toEqual(0);
+      });
+      it("if array is not an array", () => {
+        expect(() => solution({})).toThrow(TypeError);
+      });
     });
   });
 }
@@ -40,4 +54,14 @@ for (const solution of [
 // minified solution for testing your tests
 
 // prettier-ignore
-function secretSolution(a) { if (!Array.isArray(a)) { throw new TypeError("arr is not an array"); } const b = a.some(a => "string" != typeof a); if (b) { throw new TypeError("arr contains non-strings"); } const c = a.map(a => +a).filter(a => !Number.isNaN(a)).reduce((a, b) => a + b, 0); return c }
+function secretSolution(a) {
+   if (!Array.isArray(a)) {
+     throw new TypeError("arr is not an array"); 
+    } 
+    const b = a.some(a => typeof a !== "string"); // "string" != typeof a
+    if (b) {
+       throw new TypeError("arr contains non-strings");
+      } 
+      const c = a.map(a => +a).filter(a => !Number.isNaN(a)).reduce((a, b) => a + b, 0); 
+      return c 
+    }
